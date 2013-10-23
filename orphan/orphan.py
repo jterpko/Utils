a81abe2e26eee3a7306b0fb665cc50955396615a
Kenny Gorman <kgorman@objectrocket.com>
2013-10-23 16:59:37 -0500

    wip

--- orphan/orphan.py
+++ orphan/orphan.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 #
-# create a report of orphan chunks in a cluster
+# create a report of orphan documents in a cluster
+#
+# creates a database called _orphandocs and populates with documents that are orphaned
+# from the config servers point of view
+#
 #
 
 import pymongo
 import bson
+import datetime
+from pymongo import MongoReplicaSetClient
 from pymongo import MongoClient
 
 class Orphan( object ):
@@ -20,39 +26,60 @@ class Orphan( object ):
 
         return all_chunks
 
+    def saveBadChunk(self, doc, chunk, hostname, port):
+
+        bad_chunk_db = self.config_connection['_orphandocs']
+        bad_chunk_collection = bad_chunk_db['orphandocs']
+
+        master_document = {"host":hostname, "port":port, "doc":doc, "chunk":chunk, "thedate":datetime.datetime.utcnow()}
+
+        try:
+            bad_chunk_collection.save(master_document)
+        except Exception, e:
+            print e
+
+        return True
+
     def queryForChunk(self, hostname, port, chunkdata):
 
-        thecount = 0
+        thecount = -1
+        query_doc = {}
 
         connection = MongoClient(hostname, int(port))
         connection['admin'].authenticate('dba','dba')
 
         (d, c) = chunkdata['ns'].split('.')
 
-        database = connection['d']
-        collection = database['c']
+        database = connection[d]
+        collection = database[c]
 
-        #print "chunk:%s checking %s for max:%s min:%s\n" % (chunkdata['_id'], port, chunkdata['max'], chunkdata['min'])
+        shard_key = chunkdata['min'].keys()[0]
 
-        # this annoys me, is there a better way?
+        if isinstance(chunkdata['max'][shard_key], bson.max_key.MaxKey):
+            query_doc = { shard_key:{"$gte": chunkdata['min'][shard_key] } }
 
-        if isinstance(chunkdata['max']['_id'], bson.max_key.MaxKey):
-            query_doc = {"$gte": chunkdata['min'] }
-        elif isinstance(chunkdata['min']['_id'], bson.min_key.MinKey):
-            query_doc = { "$lte": chunkdata['max']}
+        elif isinstance(chunkdata['min'][shard_key], bson.min_key.MinKey):
+            query_doc = { shard_key:{ "$lte": chunkdata['max'][shard_key] } }
+
         else:
-            query_doc = { "$gte": chunkdata['min'], "$lte": chunkdata['max'] }
+            query_doc = { shard_key:{ "$gte": chunkdata['min'][shard_key], "$lte": chunkdata['max'][shard_key] } }
 
-        print "   query:"+str(query_doc)
+        print "chunk:%s checking %s with query:%s" % (chunkdata['_id'], port, query_doc)
 
-        try:
-            thecount = collection.find(query_doc).count()
-        except:
-            print "      hrm, we too low"
+        bad_documents = collection.find(query_doc)
+        thecount = bad_documents.count()
+        for bad_document in bad_documents:
+            self.saveBadChunk(bad_document, chunkdata['_id'], hostname, port)
 
-        print "   found: %i" % thecount
+        print "found: %i" % thecount
 
         return thecount
+
+    def getPrimary(self, hostname, port):
+        connection = MongoReplicaSetClient(hostname, int(port))
+        connection['admin'].authenticate('dba','dba')
+
+        return connection.primary
 
     def getOppositeShards(self, shardName):
         """ returns all shards that this chunks is *NOT* part of """
@@ -69,29 +96,28 @@ class Orphan( object ):
     def parseShardStr(self, shardStr):
         """ I hate string processing, takes string and parses to array of dics with host and port keys """
 
-        out = []
-        hosts = shardStr.split('/')[1].split(',')
+        seedstr = shardStr.split('/')[1].split(',')[0]
+        replicaset = shardStr.split('/')[0]
 
-        for i in hosts:
-            (host, port) = i.split(':')
-            doc = {"host":host, "port":port}
-            out.append(doc)
+        connection = MongoReplicaSetClient(seedstr, replicaSet = replicaset)
+        connection['admin'].authenticate('dba','dba')
 
-        return out
+        return connection.primary
 
     def checkForOrphans(self):
 
         orphan_chunk_count = 0
         for chunk in self.getChunks():
 
+            print "\n\n-----"
             print "processing chunk %s" % chunk['_id']
 
             shards_to_visit = self.getOppositeShards(chunk['shard'])
             for shard in shards_to_visit:
-                hosts = self.parseShardStr(shard)
-                for host in hosts:
-                    print "   checking host: %s" % host
-                    orphan_chunk_count += self.queryForChunk( host['host'], host['port'], chunk)
+
+                (host, port) = self.parseShardStr(shard)
+                print "\nchecking host: %s" % host
+                orphan_chunk_count += self.queryForChunk( host, port, chunk)
 
         return orphan_chunk_count
 
